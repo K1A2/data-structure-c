56d89272d54ed8e966bcb084dc7af6c80cea22aa
K1A2 <jckim0307@gmail.com>
2024-04-06 05:08:05 +0900

    dynamic array - insertAt, removeAt

--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -25,6 +25,36 @@ void insertArray(Array *a, void *element) {
     a->used++;
 }
 
+void insertAtIndexArray(Array *a, size_t index, void *element) {
+    if (index > a->used) return;
+
+    if (a->used == a->size) {
+        a->size *= 2;
+        a->array = realloc(a->array, a->size * a->elemSize);
+    }
+    memmove((char*)a->array + ((index + 1) * a->elemSize), 
+            (char*)a->array + (index * a->elemSize), 
+            (a->used - index) * a->elemSize);
+    memcpy((char*)a->array + (index * a->elemSize), element, a->elemSize);
+    a->used++;
+}
+
+void removeAtIndexArray(Array *a, size_t index) {
+    if (index >= a->used) return;
+
+    memmove((char*)a->array + (index * a->elemSize),
+            (char*)a->array + ((index + 1) * a->elemSize),
+            (a->used - index - 1) * a->elemSize);
+    a->used--;
+}
+
+void printArray(Array *a) {
+    for (size_t i = 0;i < a->used;i++) {
+        printf("%d ", *((int*)a->array + i));
+    }
+    printf("\n");
+}
+
 void freeArray(Array *a) {
     free(a->array);
     a->array = NULL;
@@ -33,18 +63,35 @@ void freeArray(Array *a) {
 
 int main() {
     Array array;
+
+    char command[10];
+    int number;
+    size_t index;
+
     initArray(&array, 5, sizeof(int));
 
-    int numbers[] = {1,2,3,4,5,6,7,8,9,10};
-    for (int i = 0;i < 10;i++) {
-        insertArray(&array, &numbers[i]);
-    }
+    printf("Commands: add <num>, insert <index> <num>, del <index>, print, exit\n");
+    while (1) {
+        printf("> ");
+        scanf("%s", command);
 
-    printf("array elements:\n");
-    for (size_t i = 0;i < array.used;i++) {
-        printf("%d ", *((int*)array.array + i));
+        if (strcmp(command, "add") == 0) {
+            scanf("%d", &number);
+            insertArray(&array, &number);
+        } else if (strcmp(command, "insert") == 0) {
+            scanf("%zu %d", &index, &number);
+            insertAtIndexArray(&array, index, &number);
+        } else if (strcmp(command, "del") == 0) {
+            scanf("%zu", &index);
+            removeAtIndexArray(&array, index);
+        } else if (strcmp(command, "print") == 0) {
+            printArray(&array);
+        } else if (strcmp(command, "exit") == 0) {
+            break;
+        } else {
+            printf("Invalid command.\n");
+        }
     }
-    printf("\n");
 
     freeArray(&array);
 
